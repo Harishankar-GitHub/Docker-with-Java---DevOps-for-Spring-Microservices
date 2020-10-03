115a0caef0e8a23607dfa880e06b9fc520bdac62
HARISHANKAR BHAT R <rhsb1997@gmail.com>
2020-10-03 20:17:50 +0530

    Docker with Java Spring Boot Hello World Rest API
    - Building docker image manually
    - Using Dockerfile to build image
    - Using Dockerfile Spotify plugin to build image
    - Creating generic reusable docker file
    - Using JIB plugin to build image
    - Using Fabric8 Docker Maven Plugin to build image

---
+++ 01-hello-world-rest-api/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApp.java
@@ -0,0 +1,15 @@
+package com.in28minutes.rest.webservices.restfulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServicesApp {
+	
+	// Changing the class name as the name is too long (Not able to commit in GitHub)
+	// The name was "RestfulWebServicesApplication"
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebServicesApp.class, args);
+	}
+}
